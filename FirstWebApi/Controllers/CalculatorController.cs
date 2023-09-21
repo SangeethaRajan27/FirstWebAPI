a9784ed12a8b9f40a0f87419b15ddd122c62442a
Sangeetha Nagarajan <sangeetha.nagarajan@gavstech.com>
2023-09-21 12:07:21 +0530

    FirstWebApi

---
+++ FirstWebApi/Controllers/CalculatorController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        // api/Calculator/Add?a=10&b=5
+        //"/" becomes a new route without adding the controller 
+        [HttpGet("/add")]  
+       /* http://localhost:5295/sum?a=10&b=0*/
+        public int Add(int a, int b) //methods in c# should in uppercase
+        {
+            return a + b;
+        }
+        [HttpGet("/sum")]
+        public int Sum(int a, int b) //methods in c# should in uppercase
+        {
+            return a + b+1000;
+        }
+        // api/calculator/subtract?a=10&b=5
+        [HttpPost]
+        public int Subtract(int a, int b) //methods in c# should in uppercase
+        {
+            return a - b;
+        }
+        // api/calculator/multiply?a=10&b=5
+        [HttpPut]
+        public int Multiply(int a, int b) //methods in c# should in uppercase
+        {
+            return a * b;
+        }
+        // api/calculator/divide?a=10&b=5
+        [HttpDelete]
+        public int Divide(int a, int b) //methods in c# should in uppercase
+        {
+            return a / b;
+        }
+    }
+}
---
+++ FirstWebApi/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet("/sum1")]
+        //AmbiguousMatchException: The request matched multiple endpoints.
+        public int Sum(int a, int b) //methods in c# should in uppercase
+        {
+            return a + b + 1000;
+        }
+        [HttpGet("demo/sum")]
+        //we need to add controller name to be as unique indentifier
+        //AmbiguousMatchException: The request matched multiple endpoints.
+        public int Sum1(int a, int b) //methods in c# should in uppercase
+        {
+            return a + b + 1000;
+        }
+    }
+}
